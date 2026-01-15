07a743c0e07ba52844884f34e9633a1701f78a01
sherytech-inc <sheryy1o1@gmail.com>
2026-01-15 11:13:00 +0500

    Update theme color seed to blue
    Changed the ColorScheme seedColor from deepPurple to blue to align the app's color theme with the primarySwatch.

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         title: 'Quiz App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
           useMaterial3: true,
         ),
         home: const OnboardingScreen(), // Start with Onboarding
